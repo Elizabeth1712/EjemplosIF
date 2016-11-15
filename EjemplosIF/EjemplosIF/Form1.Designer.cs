5677a2e0b4283e02b470f53d7dbaddfdf8b094f4
u01d164 <u01d164@SCNPC-L33055>
2016-11-14 20:40:19 -0600

    Primera prueba de if exitosa

---
+++ EjemplosIF/EjemplosIF/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace EjemplosIF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.L1 = new System.Windows.Forms.TextBox();
+            this.L2 = new System.Windows.Forms.TextBox();
+            this.L3 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // L1
+            // 
+            this.L1.Location = new System.Drawing.Point(73, 25);
+            this.L1.Name = "L1";
+            this.L1.Size = new System.Drawing.Size(100, 20);
+            this.L1.TabIndex = 0;
+            // 
+            // L2
+            // 
+            this.L2.Location = new System.Drawing.Point(73, 66);
+            this.L2.Name = "L2";
+            this.L2.Size = new System.Drawing.Size(100, 20);
+            this.L2.TabIndex = 1;
+            // 
+            // L3
+            // 
+            this.L3.Location = new System.Drawing.Point(73, 109);
+            this.L3.Name = "L3";
+            this.L3.Size = new System.Drawing.Size(100, 20);
+            this.L3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(288, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 28);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Lado 1";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(21, 71);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(40, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Lado 2";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(23, 115);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(40, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Lado 3";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(45, 177);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(67, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Validar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(144, 177);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "Salir";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.L3);
+            this.Controls.Add(this.L2);
+            this.Controls.Add(this.L1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox L1;
+        private System.Windows.Forms.TextBox L2;
+        private System.Windows.Forms.TextBox L3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ EjemplosIF/EjemplosIF/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EjemplosIF
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (Convert.ToInt16(L1.Text) > Convert.ToInt16(L2.Text) & Convert.ToInt16(L1.Text) > Convert.ToInt16(L3.Text)) 
+            {
+                MessageBox.Show("L1 es mayor");
+            }
+            else if (Convert.ToInt16(L2.Text) > Convert.ToInt16(L1.Text) & Convert.ToInt16(L2.Text) > Convert.ToInt16(L3.Text))
+            {
+                MessageBox.Show("L2 es mayor");
+            }
+            else if (Convert.ToInt16(L3.Text) > Convert.ToInt16(L1.Text) & Convert.ToInt16(L3.Text) > Convert.ToInt16(L2.Text))
+            {
+                MessageBox.Show("L3 es mayor");
+            }
+            else if (Convert.ToInt16(L3.Text) == Convert.ToInt16(L1.Text) &
+                     Convert.ToInt16(L3.Text) == Convert.ToInt16(L2.Text) &
+                     Convert.ToInt16(L2.Text) == Convert.ToInt16(L1.Text))
+            {
+                MessageBox.Show("El triángulo es equilatero");
+            }
+            else
+            {
+                MessageBox.Show("No hay un lado mayor");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+            
+        }
+    }
+}
